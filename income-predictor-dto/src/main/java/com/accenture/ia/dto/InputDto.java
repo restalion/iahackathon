8604ba22c2e42412649b29047a399a8da9044b6c
Palma Vázquez, Julio <julio.palma.vazquez@accenture.com>
2018-01-19 09:51:47 +0100

    Add marital status and label

--- income-predictor-dto/src/main/java/com/accenture/ia/dto/InputDto.java
+++ income-predictor-dto/src/main/java/com/accenture/ia/dto/InputDto.java
@@ -13,16 +13,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class InputDto implements Serializable {
 
-	/** _c1 - Age: continuous*/
+	/** _c0 - Age: continuous*/
 	Double age;
-	/** _c2 - Workclass: Private, Self-emp-not-inc, Self-emp-inc, Federal-gov, Local-gov, State-gov, Without-pay, Never-worked */
+	/** _c1 - Workclass: Private, Self-emp-not-inc, Self-emp-inc, Federal-gov, Local-gov, State-gov, Without-pay, Never-worked */
 	String workclass;
-	/** _c3 - Sampling weight: continuous */
+	/** _c2 - Sampling weight: continuous */
 	Double fnlwgt;
-	/** _c4 - education: Bachelors, Some-college, 11th, HS-grad, Prof-school, Assoc-acdm, Assoc-voc, 9th, 7th-8th, 12th, Masters, 1st-4th, 10th, Doctorate, 5th-6th, Preschool */
+	/** _c3 - education: Bachelors, Some-college, 11th, HS-grad, Prof-school, Assoc-acdm, Assoc-voc, 9th, 7th-8th, 12th, Masters, 1st-4th, 10th, Doctorate, 5th-6th, Preschool */
 	String education;
-	/** _c5 - education-num: continuous */
+	/** _c4 - education-num: continuous */
 	String educationNum;
+	/** _c5 - marital-status: Married-civ-spouse, Divorced, Never-married, Separated, Widowed, Married-spouse-absent, Married-AF-spouse. */
+	String maritalStatus;
 	/** _c6 - occupation: Tech-support, Craft-repair, Other-service, Sales, Exec-managerial, Prof-specialty, Handlers-cleaners, Machine-op-inspct, Adm-clerical, Farming-fishing, Transport-moving, Priv-house-serv, Protective-serv, Armed-Forces.*/
 	String occupation;
 	/** _c7 - relationship: Wife, Own-child, Husband, Not-in-family, Other-relative, Unmarried. */
@@ -39,4 +41,6 @@ public class InputDto implements Serializable {
 	Double hoursPerWeek;
 	/** _c13 - native-country: United-States, Cambodia, England, Puerto-Rico, Canada, Germany, Outlying-US(Guam-USVI-etc), India, Japan, Greece, South, China, Cuba, Iran, Honduras, Philippines, Italy, Poland, Jamaica, Vietnam, Mexico, Portugal, Ireland, France, Dominican-Republic, Laos, Ecuador, Taiwan, Haiti, Columbia, Hungary, Guatemala, Nicaragua, Scotland, Thailand, Yugoslavia, El-Salvador, Trinadad&Tobago, Peru, Hong, Holand-Netherlands.*/
 	String nativeCountry;
+	/** label: >50K, <=50K. */
+	String label;
 }
